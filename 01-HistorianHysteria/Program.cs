f50e92a6e6a59c4844e54a6ee43453e17eaa754f
Juan José Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2024-12-06 19:16:18 -0300

    Initial commit - Day one

---
+++ 01-HistorianHysteria/Program.cs
@@ -0,0 +1,28 @@
+﻿string inputPath = "Input.txt";
+
+List<int> leftList = new List<int>();
+List<int> rightList = new List<int>();
+
+foreach (string line in File.ReadLines(inputPath))
+{
+    if (!string.IsNullOrWhiteSpace(line))
+    {
+        string[] parts = line.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+        if (parts.Length == 2)
+        {
+            if (int.TryParse(parts[0], out int leftNumber) && int.TryParse(parts[1], out int rightNumber))
+            {
+                leftList.Add(leftNumber);
+                rightList.Add(rightNumber);
+            }
+        }
+    }
+}
+
+leftList.Sort();
+rightList.Sort();
+
+var result = leftList.Select((x, idx) => Math.Abs(rightList[idx] - x)).Sum();
+
+Console.WriteLine($"Result: {result}");
+Console.WriteLine("Exit");
