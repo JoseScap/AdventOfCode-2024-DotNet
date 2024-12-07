f8f39f07a4871eed0426acf2fd74b1501066e3d7
Juan José Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2024-12-07 01:00:04 -0300

    Refactor dayone

--- 01-HistorianHysteria/Program.cs
+++ 01-HistorianHysteria/Program.cs
@@ -1,28 +1,43 @@
-﻿string inputPath = "Input.txt";
-
-List<int> leftList = new List<int>();
-List<int> rightList = new List<int>();
-
-foreach (string line in File.ReadLines(inputPath))
+﻿namespace _01_HistorianHysteria
 {
-    if (!string.IsNullOrWhiteSpace(line))
+    internal class Program
     {
-        string[] parts = line.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
-        if (parts.Length == 2)
+        static string INPUTPATH = "Input.txt";
+
+        static int Main(string[] args)
         {
-            if (int.TryParse(parts[0], out int leftNumber) && int.TryParse(parts[1], out int rightNumber))
-            {
-                leftList.Add(leftNumber);
-                rightList.Add(rightNumber);
-            }
+            (var leftList, var rightList) = ReadInput();
+            return GetResult(leftList, rightList);
         }
+
+        public static (List<int>, List<int>) ReadInput()
+        {
+            List<int> leftList = new List<int>();
+            List<int> rightList = new List<int>();
+
+            foreach (string line in File.ReadLines(INPUTPATH))
+            {
+                if (!string.IsNullOrWhiteSpace(line))
+                {
+                    string[] parts = line.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                    if (parts.Length == 2)
+                    {
+                        if (int.TryParse(parts[0], out int leftNumber) && int.TryParse(parts[1], out int rightNumber))
+                        {
+                            leftList.Add(leftNumber);
+                            rightList.Add(rightNumber);
+                        }
+                    }
+                }
+            }
+
+            leftList.Sort();
+            rightList.Sort();
+
+            return (leftList, rightList);
+        }
+
+        public static int GetResult(List<int> leftList, List<int> rightList) =>
+            leftList.Select((x, idx) => Math.Abs(rightList[idx] - x)).Sum();
     }
 }
-
-leftList.Sort();
-rightList.Sort();
-
-var result = leftList.Select((x, idx) => Math.Abs(rightList[idx] - x)).Sum();
-
-Console.WriteLine($"Result: {result}");
-Console.WriteLine("Exit");
